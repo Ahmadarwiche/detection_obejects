21b20ced62c7e67814c4accfb2988769d5a8619c
davidbreau <davidbreau@live.fr>
2023-06-23 11:09:12 +0200

    Feat: Streamlit fonctionnel sur les images. Bug to fix sur les vidéos

---
+++ streamlit.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration, VideoTransformerBase
+import av
+import cv2
+import os
+import tempfile
+model = YOLO('models/best.pt')
+st.set_page_config(page_title="Computer vision", page_icon="🖥️")
+
+def main():
+    st.title("🪴")
+
+    # Add file uploader
+    file = st.file_uploader("Upload Image or Video to diagnostic your plant", type=["jpg", "jpeg", "png", "mp4"])
+
+    if file is not None:
+        if file.type.startswith('image'):
+            # Process uploaded image
+            img = Image.open(file)
+            results= model(img)
+            # results = model.predict(img, conf=0.25)
+
+            # # Plot the results on the image
+            res_plotted = results[0].plot(labels=True)
+            st.image(res_plotted, caption='Predicted Image', use_column_width=True)
+
+
+        elif file.type.startswith('video'):
+            ##----version originale vvv
+            video = cv2.VideoCapture(file)
+            st.video(file)
+            results= model(video)
+            # Display predicted labels on the video frames
+            annotated_video = results.render()  # Render annotated video frames with bounding boxes and labels
+            st.video(annotated_video)
+            # res=results[0].plot()
+            # st.image(res)
+
+if __name__ == '__main__':
+    main()
